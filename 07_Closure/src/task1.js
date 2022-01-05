a78900165dbeefe311ed37e56cce79ffa707bd9a
Juliya Likhatskaya <beholderka1111@gmail.com>
2022-01-05 11:28:40 +0200

    feat: add function tickets

---
+++ 07_Closure/src/task1.js
@@ -0,0 +1,17 @@
+function tickets(person) {
+    if (Array.isArray(person)) {
+        const YES = 'YES';
+        const NO = 'NO';
+        const money = person.reduce((acc, item) => {
+            if (acc >= item - 25) {
+                return acc + 25;
+            } else {
+                return false;
+            }
+        }, 0)
+        return !money && NO || money && YES;
+    }
+    return false;
+}
+
+module.exports.tickets = tickets;
